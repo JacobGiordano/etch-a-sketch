99b793632581e21794befa0c773ea59ae31621e0
JacobGiordano <jacob.giordano12@gmail.com>
2020-11-02 06:58:11 -0800

    Add new UI elems, add styling, update js for new grid & maintaining aspect ratio on new grid creation.

--- js/main.js
+++ js/main.js
@@ -1,41 +1,54 @@
-function makeSquares(numOfSquaresWide, container) {
+function makeSquares() {
+  let container = document.getElementById("square-container");
+
+  if (document.querySelectorAll(".square").length > -1) {
+    removeSquares();
+  }
+  let numOfSquaresInput = document.getElementById("num-of-squares-across");
+  let numOfSquaresWide = numOfSquaresInput.value;
   let gridSize = numOfSquaresWide * (numOfSquaresWide * .75);
-  document.getElementById("square-container").setAttribute('style', `grid-template-columns: repeat(${numOfSquaresWide}, 1fr); grid-template-rows: repeat((${numOfSquaresWide}, 1fr); border: 4px dotted purple;`);
+
   for (let i=0; i < gridSize; i++) {
     let square = document.createElement("div");
     // square.innerText = (i + 1);
     container.appendChild(square).classList.add("square");
+    square.addEventListener("mouseover", function() {
+      square.classList.add("drawn");
+    });
+
     console.log(`Pushed square #${i}`);
+  }
+
+  document.getElementById("square-container").setAttribute('style', `grid-template-columns: repeat(${numOfSquaresWide}, 1fr); grid-template-rows: repeat((${numOfSquaresWide * .75}, 1fr);`);
+
+}
+
+function removeSquares() {
+  let squares = document.querySelectorAll(".square");
+  for (let i = 0; i < squares.length; i++) {
+    const square = squares[i];
+    square.parentNode.removeChild(square);
   }
 }
 
 function clearContainer() {
   const frame = document.getElementById("etch-a-sketch__wrapper");
-  frame.classList.add("shake");
-  const drawnSquares = document.querySelectorAll(".drawn");
-  for (let i=0; i < drawnSquares.length; i++) {
-    let thisSquare = drawnSquares[i];
-    thisSquare.classList.add("clear");
-  }
-  const timer = setTimeout(function(){
-    frame.classList.remove("shake");
-    for (let j=0; j < drawnSquares.length; j++) {
-      let thisSquare = drawnSquares[j];
-      thisSquare.classList.remove("drawn", "clear");
+  if (!frame.classList.contains("shake")) {
+    frame.classList.add("shake");
+    const drawnSquares = document.querySelectorAll(".drawn");
+    for (let i=0; i < drawnSquares.length; i++) {
+      let thisSquare = drawnSquares[i];
+      thisSquare.classList.add("clear");
     }
-    clearTimeout(timer);
-  }, 3500);
+    const timer = setTimeout(function(){
+      frame.classList.remove("shake");
+      for (let j=0; j < drawnSquares.length; j++) {
+        let thisSquare = drawnSquares[j];
+        thisSquare.classList.remove("drawn", "clear");
+      }
+      clearTimeout(timer);
+    }, 3500);
+  }
 }
 
-let container = document.getElementById("square-container");
-makeSquares(16, container);
-let squares = document.querySelectorAll(".square");
-for (let i = 0; i < squares.length; i++) {
-  const square = squares[i];
-  square.addEventListener("mouseover", function() {
-    square.classList.add("drawn");
-  });
-}
-
-let clearBtn = document.getElementById("clear-btn");
-clearBtn.addEventListener("click", clearContainer);
+makeSquares();
