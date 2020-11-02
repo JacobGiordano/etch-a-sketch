cd55e8721a45f5e241ae48d91a83e1b17d43aacd
JacobGiordano <jacob.giordano12@gmail.com>
2020-11-01 20:32:03 -0800

    Add clearing button & animation. Add 4:3 aspect ratio and square squares based on width given.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
-function makeSquares(numOfSquares, container) {
-  let gridSize = numOfSquares * numOfSquares;
+function makeSquares(numOfSquaresWide, container) {
+  let gridSize = numOfSquaresWide * (numOfSquaresWide * .75);
+  document.getElementById("square-container").setAttribute('style', `grid-template-columns: repeat(${numOfSquaresWide}, 1fr); grid-template-rows: repeat((${numOfSquaresWide}, 1fr); border: 4px dotted purple;`);
   for (let i=0; i < gridSize; i++) {
     let square = document.createElement("div");
     // square.innerText = (i + 1);
@@ -8,14 +9,33 @@ function makeSquares(numOfSquares, container) {
   }
 }
 
+function clearContainer() {
+  const frame = document.getElementById("etch-a-sketch__wrapper");
+  frame.classList.add("shake");
+  const drawnSquares = document.querySelectorAll(".drawn");
+  for (let i=0; i < drawnSquares.length; i++) {
+    let thisSquare = drawnSquares[i];
+    thisSquare.classList.add("clear");
+  }
+  const timer = setTimeout(function(){
+    frame.classList.remove("shake");
+    for (let j=0; j < drawnSquares.length; j++) {
+      let thisSquare = drawnSquares[j];
+      thisSquare.classList.remove("drawn", "clear");
+    }
+    clearTimeout(timer);
+  }, 3500);
+}
+
 let container = document.getElementById("square-container");
 makeSquares(16, container);
-
 let squares = document.querySelectorAll(".square");
-
 for (let i = 0; i < squares.length; i++) {
   const square = squares[i];
   square.addEventListener("mouseover", function() {
-    square.classList.add("hovered");
+    square.classList.add("drawn");
   });
 }
+
+let clearBtn = document.getElementById("clear-btn");
+clearBtn.addEventListener("click", clearContainer);
